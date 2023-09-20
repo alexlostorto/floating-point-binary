6cdcea52f3c742fe3bd366f104065fc656490d85
Alex lo Storto <lostorto.business@gmail.com>
2023-09-20 21:45:16 +0100

    Add main functionality

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">Floating Point Binary</h1>
+
+<p align="center">
+  <b>Mantissa is veryyy confusing.</b>
+</p>
+
+<!-- <p align="center"><img width="100%" src=".github/desktop.png" alt="desktop version of the floating point binary calculator" /></p> -->
+
+## ✨ Features
+
+- Floating point binary calculator created using html, css and vanilla javascript.
+- Uses bootstrap
+- Allows 16 bit fixed-point binary number
+
+## 📋 To-do List
+
+- Make responsive
+
+## 📜 Credits
+
+Everything is coded by Alex lo Storto unless otherwise noted
+
+Licensed under the MIT License.
--- static/app.js
+++ static/app.js
@@ -1,42 +1,118 @@
-const mantissaInputs = document.querySelectorAll("#mantissa input");
-const exponentInputs = document.querySelectorAll("#exponent input");
-const binaryOutput = document.querySelector("#value");
+const denaryOutput = document.querySelector("#value");
+const convertButton = document.querySelector(".convert input");
 
-mantissaInputs.forEach(binaryInput => {
+document.querySelectorAll("#binary input").forEach(binaryInput => {
     binaryInput.addEventListener("input", updateOutput);
 });
 
-exponentInputs.forEach(binaryInput => {
+document.querySelectorAll("#mantissa input").forEach(binaryInput => {
+    binaryInput.addEventListener("input", updateOutput);
+});
+
+document.querySelectorAll("#exponent input").forEach(binaryInput => {
     binaryInput.addEventListener("input", updateOutput);
 });
 
 function updateOutput() {
-    const mantissa = calculateMantissa();
-    const exponent = calculateExponent();
-
-    output = mantissa * Math.pow(2, exponent);
-
-    binaryOutput.textContent = output;
+    if (window.getComputedStyle(document.querySelector("#binary-section")).getPropertyValue('display') == "block") {
+        const binary = calculateBinary();
+        denaryOutput.textContent = binary;
+    } else {
+        const mantissa = calculateMantissa();
+        const exponent = calculateExponent();
+        output = mantissa * Math.pow(2, exponent);
+        denaryOutput.textContent = output;
+    }
 }
 
 function calculateExponent() {
-    const binaryValues = [-16, 8, 4, 2, 1];
+    const exponentInputs = document.querySelectorAll("#exponent input");
+    const exponentLabels = document.querySelectorAll("#exponent label");
     let output = 0;
 
     for (i=0; i<exponentInputs.length; i++) {
-        output += exponentInputs[i].value * binaryValues[i];
+        output += exponentInputs[i].value * eval(exponentLabels[i].textContent);
     }
 
     return output;
 }
 
 function calculateMantissa() {
-    const binaryValues = [-1, 1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 1/128];
+    const mantissaInputs = document.querySelectorAll("#mantissa input");
+    const mantissaLabels = document.querySelectorAll("#mantissa label");
     let output = 0;
 
     for (i=0; i<mantissaInputs.length; i++) {
-        output += mantissaInputs[i].value * binaryValues[i];
+        output += mantissaInputs[i].value * eval(mantissaLabels[i].textContent);
     }
 
     return output;
 }
+
+function calculateBinary() {
+    const binaryInputs = document.querySelectorAll("#binary input");
+    const binaryLabels = document.querySelectorAll("#binary label");
+    let output = 0;
+
+    for (i=0; i<binaryInputs.length; i++) {
+        output += binaryInputs[i].value * eval(binaryLabels[i].textContent);
+    }
+
+    return output;
+}
+
+function updateMantissa() {
+    const binary = [...document.querySelectorAll("#binary-section input")].map(element => element.value);
+    const mantissaInputs = document.querySelectorAll("#mantissa input");
+    const exponent = binary.length - 1;
+
+    for (i=0; i<binary.length; i++) {
+        mantissaInputs[i].value = binary[i];
+    }
+
+    updateExponent(exponent);
+}
+
+function updateExponent(exponent) {
+    binaryValue = convertToTwosComplement(exponent);
+    const exponentInputs = document.querySelectorAll("#exponent input");
+
+    for (i=0; i<binaryValue.length; i++) {
+        exponentInputs[i].value = binaryValue[i];
+    }
+}
+
+function convertToTwosComplement(decimal) {
+    let binaryValue = [0, 0, 0, 0, 0];
+
+    if (decimal < -16 || decimal > 15) {
+        alert("Decimal exponent out of range for 5-bit two's complement representation.");
+        return binaryValue;
+    }
+
+    if (decimal < 0) {
+        decimal = decimal + 16;
+    }
+
+    let counter = 4;
+    
+    while (decimal > 0) {
+        binaryValue[counter] = decimal % 2 == 1 ? 1 : 0;
+        decimal = Math.floor(decimal / 2);
+        counter --;
+    }
+
+    return binaryValue;
+}
+
+convertButton.addEventListener("change", () => {
+    if (convertButton.checked) {
+        document.querySelector("#binary-section").style.display = "none";
+        document.querySelector("#mantissa-section").style.display = "block";
+        updateMantissa();
+        updateOutput();
+    } else {
+        document.querySelector("#binary-section").style.display = "block";
+        document.querySelector("#mantissa-section").style.display = "none";
+    }
+})
