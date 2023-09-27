f3c54b4cf8bd3d9ff07b1796143e033f5f68b46e
Alex lo Storto <lostorto.business@gmail.com>
2023-09-27 18:23:28 +0100

    Finish static version

--- static/app.js
+++ static/app.js
@@ -1,6 +1,11 @@
 const denaryOutput = document.querySelector("#value");
 const convertButton = document.querySelector(".convert input");
 
+const addBinaryLeft = document.querySelector("#add-binary-left");
+const subtractBinaryLeft = document.querySelector("#subtract-binary-left");
+const addBinaryRight = document.querySelector("#add-binary-right");
+const subtractBinaryRight = document.querySelector("#subtract-binary-right");
+
 document.querySelectorAll("#binary input").forEach(binaryInput => {
     binaryInput.addEventListener("input", updateOutput);
 });
@@ -12,6 +17,99 @@ document.querySelectorAll("#mantissa input").forEach(binaryInput => {
 document.querySelectorAll("#exponent input").forEach(binaryInput => {
     binaryInput.addEventListener("input", updateOutput);
 });
+
+document.querySelector("#add-binary-left").addEventListener("click", () => {
+    const mostSignificantBit = document.querySelector("#binary label").textContent.replace("-", "");
+    document.querySelector("#binary label").textContent = mostSignificantBit;
+    newBinaryInput = createInput("-" + mostSignificantBit * 2);
+
+    const firstChild = document.querySelector("#binary section").firstChild;
+    document.querySelector("#binary section").insertBefore(newBinaryInput, firstChild);
+
+    if (window.getComputedStyle(document.querySelector("#subtract-binary-left")).getPropertyValue('display') == "none") {
+        document.querySelector("#subtract-binary-left").style.display = "block";
+    }
+});
+
+document.querySelector("#subtract-binary-left").addEventListener("click", () => {
+    if (document.querySelectorAll("#binary section div").length == 1) return;
+
+    document.querySelector("#binary section div").remove();
+    mostSignificantBit = document.querySelector("#binary section label").textContent;
+    document.querySelector("#binary section label").textContent = "-" + mostSignificantBit;
+
+    if (mostSignificantBit == "1") {
+        document.querySelector("#subtract-binary-left").style.display = "none";
+    }
+});
+
+document.querySelector("#add-binary-right").addEventListener("click", () => {
+    const parentElement = document.querySelector("#binary section").lastElementChild;
+    const leastSignificantBit = parentElement.querySelector("label").textContent.replace("-", "");
+    let fraction;
+
+    if (leastSignificantBit == 1) {
+        fraction = [1, 1];
+    } else {
+        fraction = leastSignificantBit.split('/');
+    }
+
+    newBinaryInput = createFractionalInput(fraction[0] + "/" + fraction[1] * 2);
+    document.querySelector("#binary section").appendChild(newBinaryInput);
+
+    if (window.getComputedStyle(document.querySelector("#subtract-binary-right")).getPropertyValue('display') == "none") {
+        document.querySelector("#subtract-binary-right").style.display = "block";
+    }
+});
+
+document.querySelector("#subtract-binary-right").addEventListener("click", () => {
+    if (document.querySelectorAll("#binary section div").length == 1) return;
+    
+    leastSignificantBit = document.querySelector("#binary section").lastElementChild;
+    leastSignificantBit.remove();
+    if (leastSignificantBit.querySelector("label").textContent == "1/2") {
+        document.querySelector("#subtract-binary-right").style.display = "none";
+    }
+});
+
+function createInput(value) {
+    const container = document.createElement("div");
+
+    const label = document.createElement("label");
+    label.for = `binary-${value.replace("-", "")}`;
+    label.textContent = value;
+
+    const input = document.createElement("input");
+    input.type = "number";
+    input.id = `binary-${value.replace("-", "")}`;
+    input.addEventListener("input", updateOutput);
+
+    container.appendChild(label);
+    container.appendChild(input);
+
+    return container;
+}
+
+function createFractionalInput(value) {
+    const container = document.createElement("div");
+
+    const fraction = value.replace("-", "").split('/');
+    const className = `${fraction[0]}-over-${fraction[1]}`;
+
+    const label = document.createElement("label");
+    label.for = `binary-${className}`;
+    label.textContent = value;
+
+    const input = document.createElement("input");
+    input.type = "number";
+    input.id = `binary-${className}`;
+    input.addEventListener("input", updateOutput);
+
+    container.appendChild(label);
+    container.appendChild(input);
+
+    return container;
+}
 
 function updateOutput() {
     if (window.getComputedStyle(document.querySelector("#binary-section")).getPropertyValue('display') == "block") {
@@ -62,12 +160,29 @@ function calculateBinary() {
 }
 
 function updateMantissa() {
-    const binary = [...document.querySelectorAll("#binary-section input")].map(element => element.value);
-    const mantissaInputs = document.querySelectorAll("#mantissa input");
-    const exponent = binary.length - 1;
+    const binaryValues = [...document.querySelectorAll("#binary-section input")].map(element => element.value);
+    const binaryLabels = [...document.querySelectorAll("#binary-section label")].map(element => element.textContent);
+    let exponent = -1;
 
-    for (i=0; i<binary.length; i++) {
-        mantissaInputs[i].value = binary[i];
+    binaryLabels.forEach((element, _) => {
+        if (!(element.includes("/"))) {
+            exponent += 1;
+        }
+    })
+
+    while (binaryValues.length > document.querySelectorAll("#mantissa input").length) {
+        const parentElement = document.querySelector("#mantissa").lastElementChild;
+        const leastSignificantBit = parentElement.querySelector("label").textContent.replace("-", "");
+        const fraction = leastSignificantBit.split('/');
+    
+        newBinaryInput = createFractionalInput(fraction[0] + "/" + fraction[1] * 2);
+        document.querySelector("#mantissa").appendChild(newBinaryInput);
+    }
+
+    const mantissaInputs = document.querySelectorAll("#mantissa input");
+
+    for (i=0; i<binaryValues.length; i++) {
+        mantissaInputs[i].value = binaryValues[i];
     }
 
     updateExponent(exponent);
@@ -114,5 +229,6 @@ convertButton.addEventListener("change", () => {
     } else {
         document.querySelector("#binary-section").style.display = "block";
         document.querySelector("#mantissa-section").style.display = "none";
+        updateOutput();
     }
 })
